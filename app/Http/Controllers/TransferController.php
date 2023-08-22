5b25b44fb4ffcce62c5a5babf17e4832a168e9f8
LuanMobile <luanhenriique.ls13@gmail.com>
2023-08-22 08:35:33 -0300

    Codigo refatorado e desafio finalizado

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -13,9 +13,8 @@ class TransferController extends Controller
 
         $user = User::find($id);
         $amount = Saldos::where('user_id', $id)->first();
-        $saldoAtual = $amount->amount;
 
-        return response()->json("Saldo Atual do usuário " . $user->name . " é R$ ". $saldoAtual);
+        return response()->json(["Saldo Atual do usuário " . $user->name . " é R$ " => $amount->amount . " reais!"]);
     }
 
     public function getValue(Request $req, $id1, $id2) {
@@ -23,26 +22,17 @@ class TransferController extends Controller
         $saldoId1 = Saldos::find($id1);
         $saldoId2 = Saldos::find($id2);
         $reqAmount = $req->all();
-        $amountId = $saldoId1->amount;
-        $amountReq = $reqAmount['amount'];
 
-        //valida se o usuario tem saldo suficiente antes da transferencia
-        if($amountId >= $amountReq) {
-            
-            //dd($reqAmount['amount']);
-            //logica da transferencia por id na url: {id1}/{id2} e no corpo da req, o valor monetario
-            $saldoId1->amount = $saldoId1->amount - $reqAmount['amount'];
-            $saldoId1->save();
-            $saldoId2->amount = $saldoId2->amount + $reqAmount['amount'];
-            $saldoId2->save();
-
-            return response()->json("Transferência realizada com sucesso, no valor de R$" . $reqAmount['amount'] . " reais!");
-        } else {
+        if($saldoId1->amount < $reqAmount['amount']) {
 
             return response()->json("Erro: Saldo insuficiente para realizar transferencia!", 400);
         }
 
-        //valida se o usuario tem saldo suficiente antes da transferencia
-
+        $saldoId1->amount -= $reqAmount['amount'];
+        $saldoId1->save();
+        $saldoId2->amount += $reqAmount['amount'];
+        $saldoId2->save();
+    
+        return response()->json("Transferência realizada com sucesso, no valor de R$ " . $reqAmount['amount'] . " reais!");
     }
 }

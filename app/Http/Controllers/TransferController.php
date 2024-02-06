8018ba2f8ce5db2ee8dd86e8bffbaeaa6c8921d8
LuanMobile <luanhenriique.ls13@gmail.com>
2024-02-06 13:26:31 -0300

    ajustando logica de transferencia de saldos

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -13,15 +13,18 @@ class TransferController extends Controller
 
         $user = User::find($id);
         $amount = Saldos::where('user_id', $id)->first();
+        if (empty($amount)) {
+            return response()->json("Usuário não possui Saldo!");
+        }
         $saldoAtual = $amount->amount;
 
-        return response()->json("Saldo Atual do usuário " . $user->name . " é R$ ". $saldoAtual);
+        return response()->json("Saldo atual do usuário " . $user->name . " é de R$ ". $saldoAtual . " reais!");
     }
 
     public function getValue(Request $req, $id1, $id2) {
 
-        $saldoId1 = Saldos::find($id1);
-        $saldoId2 = Saldos::find($id2);
+        $saldoId1 = Saldos::where('user_id', $id1)->first();
+        $saldoId2 = Saldos::where('user_id', $id2)->first();
         $reqAmount = $req->all();
         $amountId = $saldoId1->amount;
         $amountReq = $reqAmount['amount'];
@@ -29,7 +32,6 @@ class TransferController extends Controller
         //valida se o usuario tem saldo suficiente antes da transferencia
         if($amountId >= $amountReq) {
             
-            //dd($reqAmount['amount']);
             //logica da transferencia por id na url: {id1}/{id2} e no corpo da req, o valor monetario
             $saldoId1->amount = $saldoId1->amount - $reqAmount['amount'];
             $saldoId1->save();
@@ -41,8 +43,5 @@ class TransferController extends Controller
 
             return response()->json("Erro: Saldo insuficiente para realizar transferencia!", 400);
         }
-
-        //valida se o usuario tem saldo suficiente antes da transferencia
-
     }
 }
